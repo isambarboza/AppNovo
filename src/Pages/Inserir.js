5f653580f536955f898508837d6f4e7303709087
isambarboza <isabelabarboza17@gmail.com>
2024-05-29 08:39:36 -0300

    Finalizado

--- src/Pages/Inserir.js
+++ src/Pages/Inserir.js
@@ -1,16 +1,67 @@
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import { useState } from 'react';
 import React from 'react'
 
 export default function Inserir() {
+  const [clienteNome, setClienteNome] = useState();
+  const [clienteEmail, setClienteEmail] = useState();
+  const [clienteGenere, setClienteGenere] = useState();
+
+  async function InsertClient() {
+    await fetch('http://10.139.75.23:5251/api/Clients/CreateClient/', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json; charset-UTF-8',
+      },
+      body: JSON.stringify({
+        clientEmail: clienteEmail,
+        clientGenere: clienteGenere,
+        clientName: clienteNome
+      })
+    })
+      .then((response) => response.json(), setClienteEmail(''), setClienteGenere(''), setClienteNome(''))
+      .then(json => console.log(json))
+      .catch(err => console.log(err));
+    getClientes();
+  }
   return (
-    <View style={css.container}>
-      <Text style={css.text}>Inserir</Text>
+    <View style={styles.container}>
+      <TextInput
+        inputMode="text"
+        placeholder='Nome'
+        style={styles.input}
+        value={clienteNome}
+        onChangeText={(digitado) => setClienteNome(digitado)}
+        placeholderTextColor='#BFBFBF'
+      />
+      <TextInput
+        inputMode="email"
+        placeholder='E-mail'
+        style={styles.input}
+        value={clienteEmail}
+        onChangeText={(digitado) => setClienteEmail(digitado)}
+        placeholderTextColor='#BFBFBF'
+      />
+      <TextInput
+        inputMode="text"
+        placeholder='Gênero'
+        style={styles.input}
+        value={clienteGenere}
+        onChangeText={(digitado) => setClienteGenere(digitado)}
+        placeholderTextColor='#BFBFBF'
+      />
+      <TouchableOpacity onPress={() => InsertClient()}>
+        <Text style={styles.btnCadastrar}>CADASTRAR</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.text}>Inserir</Text>
     </View>
   )
 }
-const css = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#191919",
+    width: "100%",
+    backgroundColor: "white",
     flexGrow: 1,
     color: "white",
     justifyContent: "center",
@@ -18,5 +69,22 @@ const css = StyleSheet.create({
   },
   text: {
     color: "white"
-  }
+  },
+  input: {
+    width: 300,
+    height: 45,
+    borderColor: '#bdc3c7',
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    color: '#191919',
+},
+btnCadastrar:{
+  backgroundColor: '#0195fd',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+}
 })
